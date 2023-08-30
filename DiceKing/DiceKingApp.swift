b9020968bb96633c3fec94090b0acfe68cc7e965
Thu Nguyen <101914949+npat273@users.noreply.github.com>
2023-08-30 15:54:52 +0700

    Initial Commit

---
+++ DiceKing/DiceKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceKingApp.swift
+//  DiceKing
+//
+//  Created by Thu Nguyen  on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
